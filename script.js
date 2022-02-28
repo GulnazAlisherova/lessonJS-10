e847be054eca880ed10d3e4ce2895d3d8a98c720
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-02-28 15:50:08 +0600

    added name

--- script.js
+++ script.js
@@ -1,44 +1,63 @@
-let fruits = ["apple", "banana", false, 13, "hello", ["1", "2", "3"], {name: "peter" , lastname: "parker"}, 12345]
-fruits.forEach((item=>{
-  console.log(typeof(item)); 
-}))
-// fruits.forEach(element => console.log(element));
+// let fruits = ["apple", "banana", false, 13, "hello", ["1", "2", "3"], {name: "peter" , lastname: "parker"}, 12345]
+// fruits.forEach((item=>{
+//   console.log(typeof(item)); 
+// }))
+// // fruits.forEach(element => console.log(element));
+
+// let people = [{
+//   name:"Peter",
+//   lastname:"Parker",
+//   age: 34
+// },
+// {
+//   name:"Lasley",
+//   lastname: "Johnson",
+//   age: 55
+// },
+// {
+//   name: "Sam",
+//   lastname: "Cruz",
+//   age: 11
+// },
+// {
+//   name: "Sadyr",
+//   lastname:"Japarov",
+//   age: 44
+// },
+// {
+//   name: "Elon",
+//   lastname: "Mask",
+//   age: 51
+// }
+// ]
+// let container = document.querySelector("#container")
+
+// people.forEach((item) => {
+//   let person = document.createElement("div");
+//   let name = `${item. name} `;
+//   let lastname =`${item.lastname}  `;
+//   let age = `${item.age}`;
+//   person.append( name);
+//   person.append( lastname);
+//   person.append( age);
+//   container.append( person);
+// })
+
 
 let people = [{
-  name:"Peter",
-  lastname:"Parker",
-  age: 34
+  name: "Payla",
+  resume: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, voluptatum.",
+  profession:"Java"
 },
 {
-  name:"Lasley",
-  lastname: "Johnson",
-  age: 55
+  name: "Clara",
+  resume: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, voluptatum.",
+  profession:"3D-model"
 },
 {
-  name: "Sam",
-  lastname: "Cruz",
-  age: 11
-},
-{
-  name: "Sadyr",
-  lastname:"Japarov",
-  age: 44
-},
-{
-  name: "Elon",
-  lastname: "Mask",
-  age: 51
-}
+  name: "Peter",
+  resume: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, voluptatum.",
+  profession:"front-end"
+} 
 ]
-let container = document.querySelector("#container")
 
-people.forEach((item) => {
-  let person = document.createElement("div");
-  let name = `${item. name} `;
-  let lastname =`${item.lastname}  `;
-  let age = `${item.age}`;
-  person.append( name);
-  person.append( lastname);
-  person.append( age);
-  container.append( person);
-})
