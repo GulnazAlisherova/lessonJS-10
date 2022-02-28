f5e4c293d99ca571b4ba6fb40b1d22832226a986
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-02-28 15:03:01 +0600

    added typeOf

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 let fruits = ["apple", "banana", false, 13, "hello", ["1", "2", "3"], {name: "peter" , lastname: "parker"}, 12345]
+fruits.forEach((item=>{
+  console.log(typeof(item));
+}))
+//string
+//boolean
+//integer
+//srting
+//array
+//object
+//integer
 
 // fruits.forEach(element => console.log(element)); 
-
