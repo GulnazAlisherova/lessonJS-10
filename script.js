af7d6b127a03fbc55cfc3cc84e9201dc01f315ee
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-02-28 15:05:47 +0600

    added let people

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 let fruits = ["apple", "banana", false, 13, "hello", ["1", "2", "3"], {name: "peter" , lastname: "parker"}, 12345]
 fruits.forEach((item=>{
-  console.log(typeof(item));
+  console.log(typeof(item)); 
 }))
-//string
-//boolean
-//integer
-//srting
-//array
-//object
-//integer
+// fruits.forEach(element => console.log(element));
 
-// fruits.forEach(element => console.log(element)); 
+let people = [{
+  name:"Peter",
+  lastname:"parker",
+  age: 34
+},
+{
+  name:"Lasley"
+}
+]
